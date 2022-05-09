04c65f2d5462e244918bd29216fb44fba09807ee
Luca Hyde <cogoslo@gmail.com>
2022-05-09 23:02:15 +0100

    making the command line version of the program to create a structure

---
+++ Python Work/Pre-work/Commandlineverpy.py
@@ -0,0 +1,8 @@
+import math
+
+mincount = 10
+count = input(int "how many posters do you want?")
+areaH = input(int "how tall do you want your posters")
+areaW = input(int "how high do you want your posters")
+area = decimal
+price = decimal
